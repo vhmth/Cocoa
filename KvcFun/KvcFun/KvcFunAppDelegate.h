bab6d448e9e4f17cf539ff7814b295921d88b438
Vinay <vhiremath4@gmail.com>
2013-03-16 00:53:01 -0700

    initial KvcFun code (chapter 7)

---
+++ KvcFun/KvcFun/KvcFunAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KvcFunAppDelegate.h
+//  KvcFun
+//
+//  Created by Vinay Siddharam Hiremath on 3/16/13.
+//  Copyright (c) 2013 Underpull. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface KvcFunAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ KvcFun/KvcFun/KvcFunAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  KvcFunAppDelegate.m
+//  KvcFun
+//
+//  Created by Vinay Siddharam Hiremath on 3/16/13.
+//  Copyright (c) 2013 Underpull. All rights reserved.
+//
+
+#import "KvcFunAppDelegate.h"
+
+@implementation KvcFunAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
