ff0ad71d1750925611f6985b7c04f3aadaf4b256
Vinay <vhiremath4@gmail.com>
2013-03-16 01:29:36 -0700

    initial RaiseMan commit (beginning chapter 8)

---
+++ RaiseMan/RaiseMan/RMDocument.h
@@ -0,0 +1,13 @@
+//
+//  RMDocument.h
+//  RaiseMan
+//
+//  Created by Vinay Siddharam Hiremath on 3/16/13.
+//  Copyright (c) 2013 Underpull. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RMDocument : NSDocument
+
+@end
